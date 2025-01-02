84e3569d99ca8be9847c61e3a5d60f95b270cf19
Malaya Tiwari <sidzilegend@gmail.com>
2025-01-02 11:30:52 +0530

    update: database connection with sequalize

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { router } = require("./routes/router");
+const { dbConnection } = require("./config/dbConnect");
+
 const app = express();
 
-app.use('/', router)
+app.use("/", router);
 
 app.listen(3000, () => {
   console.log(`Server is running on PORT: 3000`);
+  dbConnection();
 });
+
+
 
 // file structure
 
